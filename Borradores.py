9d046a510e6f78e5a2020cc1c71e183beb51a828
fierroxido <fierroalejandro739@gmail.com>
2022-03-24 08:54:52 -0500

    Actualización trabajos

---
+++ Borradores.py
@@ -0,0 +1,18 @@
+#Inicio
+sentence="Hola, mundoooo."
+print(sentence)
+print(sentence[:4])
+print(sentence[5:])
+print(sentence[9:])
+print(sentence[:12:2])
+
+print(sentence.startswith('Hola'))
+print(sentence.count('o'))
+print(sentence.find("o"))
+print(sentence.upper())
+
+
+
+Entrada= int(input(print("Introduzca un valor entero")))
+N=(Entrada+(Entrada*Entrada)+(Entrada*Entrada*Entrada))
+print(N)
---
+++ ejercicio6.py
@@ -0,0 +1,4 @@
+nombres= input ("Introduzca su nombre")
+apellidos= input ("Introduzca sus apellidos")
+
+print(F'{apellidos},{nombres}')
