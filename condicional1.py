23d2e1d8ba53480c08783e2a47446c125f105f0f
fierroxido <fierroalejandro739@gmail.com>
2022-03-30 11:28:21 -0500

    Create condicional1.py

---
+++ condicional1.py
@@ -0,0 +1,22 @@
+from re import A
+
+
+puede_volar=True
+es_humano=True
+tiene_mascara=True
+
+Ironman=1
+Capitan_Marvel=2
+Ronan_Accuser=3
+Vision=4
+
+superheroe=input("¿Su superheroe puede volar? ")
+if puede_volar=="si":
+    input("¿Su superheroe es humano? ")
+elif es_humano=="si":
+    input("¿Su Superheroe tiene mascara? ")   
+elif tiene_mascara=="si":
+        print("Su superheroe es", 1)
+    
+
+    
