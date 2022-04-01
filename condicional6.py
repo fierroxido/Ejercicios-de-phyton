960b92e8537ee6fe82bd554d1f7c56fffea116ab
fierroxido <fierroalejandro739@gmail.com>
2022-04-01 10:42:49 -0500

    Update condicional6.py

---
+++ condicional6.py
@@ -0,0 +1,10 @@
+descuento=input("Introduzca el color de la balota que sacó")
+producto=int (input("Introduzca el valor del producto"))
+precio=producto*descuento
+br=float(0.100)
+bv=float(0.50)
+bb=float(0.30)
+bn=float(0.20)
+ba=float(0.10)
+
+if producto<50000 :
