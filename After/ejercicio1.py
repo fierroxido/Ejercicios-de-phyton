7a8b97dd0023faaab209aec93681d6aba2abd0b5
fierroxido <fierroalejandro739@gmail.com>
2022-05-27 09:53:31 -0500

    Update ejercicio1.py

---
+++ After/ejercicio1.py
@@ -0,0 +1,32 @@
+from cmath import sqrt
+
+
+smlv=int(1000000)#Salario minimo legal vigente
+print(f"Recuerde que el salario minimo legal vigente es de {smlv}")
+smlvd=int(input("Digite el salario que usted recibe que oscile entre 1 smlv y 2 smlv: "))
+adt=int(117112)#Auxilio de transporte
+dias=int(input("Digite el número de días trabajados: "))
+slm=str(input("Digite SI, si tiene menos 2 salarios minimos legales vigentes: "))
+sd=(smlvd*dias)/30# Sueldo devengado/Salario basico
+sb=((smlvd+adt)*dias)/30 #Es el salario BASE que tendría el trabajador contando con smlv y adt
+#Ahora se agrega los datos de la comisión
+
+ventas=int(input("Digite el número de ventas que ha hecho durante el presente mes trabajando"))
+comision=ventas*0.2
+truet=(sd or sb)+comision
+descuentos=
+truesalario=print(descuentos)
+#float(truet*0.(input("Digite el costo total de los prestamos:")))
+
+  
+    
+def todo1(sd,sb):
+    if slm=="SI" or "si":
+        print(f"Se trabajará con {smlvd} para hacer la correspondiente cuenta de cobro")
+    else:
+        print(truesalario)
+
+    
+    
+    
+    
